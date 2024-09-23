33aed8adda319b51699d3f615c0ad19b49d91d1c
bobbythree <mynameisbobbylewis@hotmail.com>
2024-09-23 00:28:26 -0500

    created command prompt loop

--- src/game.py
+++ src/game.py
@@ -2,18 +2,8 @@ import pyfiglet
 from verbs import *
 from nouns import *
 
-def run_command(command):
-    if command[0] in verbs.keys() and len(command) == 1:
-        print(f"{command[0]} what?")
-    elif command[0] in verbs.keys() and command[1] in nouns.keys():
-        output = verbs[command[0]]["func"](command[1])
-        print(output)
-    else: print("Does not compute.")
-
-def start():
-    game_logo = pyfiglet.figlet_format("Q U E S T", font="colossal")
-    print(game_logo)
-    print("""Welcome to QUEST! You wake up in your bedroom which is dimly lit by artificial light coming through the [window]. In the room is your [computer] sitting on a [desk]. There is one [door] to get out.""")       
+#display command prompt, tokenize user input, get verb and noun
+def command_prompt():
     command = input("> ")
     tokens = command.lower().split()
     command_list = []
@@ -23,11 +13,25 @@ def start():
         if x in nouns.keys():
             command_list.append(x)
     run_command(command_list)
-    
-            
+
+
+def run_command(command):
+    if command[0] in verbs.keys() and len(command) == 1:
+        print(f"{command[0]} what?")
+    elif command[0] in verbs.keys() and command[1] in nouns.keys():
+        output = verbs[command[0]]["func"](command[1])
+        print(output)
+    else: print("Does not compute.")
+    command_prompt()
+
+def start():
+    game_logo = pyfiglet.figlet_format("Q U E S T", font="colossal")
+    print(game_logo)
+    print("""Welcome to QUEST! You wake up in your bedroom which is dimly lit by artificial light coming through the [window]. In the room is your [computer] sitting on a [desk]. There is one [door] to get out.""")            
 
 if __name__ == "__main__":
     start()
-    
+
+command_prompt()    
 
             
