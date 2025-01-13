437241ca022e4bc0d0ea77b18115f2b7ffed29c8
bobbythree <mynameisbobbylewis@hotmail.com>
2025-01-13 17:46:55 -0600

    move text parser to its own module. Code cleanup

--- src/game.py
+++ src/game.py
@@ -1,98 +1,11 @@
-"""this is the main py file and entry point for this program.
-
-It contains the text parser functions (command_prompt and run_command) as well
-as the game's start function.
-"""
+"""this is the main py file and entry point for this program."""
 
 
 import os
 import pyfiglet
-import player
-from verbs import verbs
-from one_word_commands import *
-from scenes import (
-    bedroom as bedroom,
-    street as street,
-    staircase as staircase
-)
+from parser import (command_prompt, run_command)
+from scenes import bedroom as bedroom
 from colors import colors
-
-
-# text-parser funcs
-def command_prompt(scene_name):
-    """display the command prompt in the current scene. tokenize user input.
-    loop over tokens. put verbs, nouns etc into command_list. Call 
-    run_command func, passing it the commands.
-    """
-
-    command = input("> ")
-    tokens = command.lower().split()
-    command_list = []
-    for x in tokens:
-        if x in verbs.keys() and x not in command_list:
-            command_list.append(x)
-        elif x in scene_name.scene["nouns"].keys() and x not in command_list:
-            command_list.append(x)
-        elif x in player.stats["inventory"] and x not in command_list:
-            command_list.append(x)
-        for i in scene_name.scene["nouns"].values():
-            if x in i["contents"]:
-                command_list.append(x)
-
-    run_command(command_list, scene_name)
-
-
-def run_command(command, scene_name):
-    """take in command list and current scene name. handle various commands
-    and errors. If no errors, execute the verb's function from verbs.py. 
-    Call the command_prompt func again."""
-
-
-    print(colors["green"])
-
-    # if no words matched known words
-    if command == []:
-        print("does not compute.")
-
-    #navigation commands
-    elif command[0] == "go" and len(command) == 1:
-        print("Go where?")
-
-    elif command[0] == "go" and command[1] in scene_name.scene["next_scene"]:
-        print(f'{colors["bold"]}Current Scene: {scene_name.scene["next_scene"][command[1]]["name"]}{colors["green"]}')
-        print(scene_name.scene["next_scene"][command[1]]["module"].scene["nouns"][command[1]]["description"])
-        print(colors["cyan"])
-        command_prompt(scene_name.scene["next_scene"][command[1]]["module"])
-
-    elif command[0] == "go" and command[1] not in str(scene_name.scene["next_scene"]):
-        print("You can't go there.")
-
-    # if no noun
-    elif command[0] in verbs.keys() and len(command) == 1:
-        print(f"{command[0]}...?")
-    elif command[0] in scene_name.scene["nouns"] and len(command) == 1:
-        print("wha??")
-
-    # if item is in inventory
-    elif command[0] in verbs.keys() and command[1] not in scene_name.scene["nouns"].keys() and command[1] in player.stats["inventory"]:
-        output = verbs[command[0]]["func"](scene_name, command[1])
-        print(output)
-
-    # both verb and noun match
-    elif command[0] in verbs.keys() and command[1] in scene_name.scene["nouns"].keys():
-        output = verbs[command[0]]["func"](scene_name, command[1])
-        print(output)
-
-    # get or describe item inside something
-    elif [output := verbs[command[0]]["func"] (scene_name, command[1]) for i in scene_name.scene["nouns"].values() if command[1] in i["contents"] and i ["is_open"]]:
-        print(output)
-
-    elif [output := verbs[command[0]]["func"] (scene_name, command[1]) for i in scene_name.scene["nouns"].values() if command[1] in i["contents"] and not i ["is_open"]]:
-        print("You do not see such an item.")
-
-    #loop back into the prompt
-    print(colors['cyan'])
-    command_prompt(scene_name)
 
 
 # start of gameplay
@@ -103,7 +16,7 @@ def clear_terminal():
 
 
 def start():
-    """display welcome message. Call command_prompt func with first scene."""
+    """display startscreen."""
 
 
     game_logo = pyfiglet.figlet_format("MAITREYA's QUEST", font="smkeyboard")
@@ -119,6 +32,8 @@ You wake up in your [bedroom] which is dimly lit by artificial light coming
 through the [window]. In the room is your [computer] sitting on a [desk].
 There is one [door] to get out.{colors['cyan']} """)
 
+
+#clear terminal and call start func
 if __name__ == "__main__":
     clear_terminal()
     start()
---
+++ src/parser.py
@@ -0,0 +1,90 @@
+"""
+This module contains the text-parser functions (command_prompt and run_command).
+"""
+
+
+import player
+from verbs import verbs
+from scenes import (
+    bedroom as bedroom,
+    street as street,
+    staircase as staircase
+)
+from colors import colors
+
+
+# text-parser funcs
+def command_prompt(scene_name):
+    """display the command prompt in the current scene. tokenize user input.
+    loop over tokens. put verbs, nouns etc into command_list. Call 
+    run_command func, passing it the commands.
+    """
+
+    command = input("> ")
+    tokens = command.lower().split()
+    command_list = []
+    for x in tokens:
+        if x in verbs.keys() and x not in command_list:
+            command_list.append(x)
+        elif x in scene_name.scene["nouns"].keys() and x not in command_list:
+            command_list.append(x)
+        elif x in player.stats["inventory"] and x not in command_list:
+            command_list.append(x)
+        for i in scene_name.scene["nouns"].values():
+            if x in i["contents"]:
+                command_list.append(x)
+
+    run_command(command_list, scene_name)
+
+
+def run_command(command, scene_name):
+    """take in command list and current scene name. handle various commands
+    and errors. If no errors, execute the verb's function from verbs.py. 
+    Call the command_prompt func again."""
+
+
+    print(colors["green"])
+
+    # if no words matched known words
+    if command == []:
+        print("does not compute.")
+
+    #navigation commands
+    elif command[0] == "go" and len(command) == 1:
+        print("Go where?")
+
+    elif command[0] == "go" and command[1] in scene_name.scene["next_scene"]:
+        print(f'{colors["bold"]}Current Scene: {scene_name.scene["next_scene"][command[1]]["name"]}{colors["green"]}')
+        print(scene_name.scene["next_scene"][command[1]]["module"].scene["nouns"][command[1]]["description"])
+        print(colors["cyan"])
+        command_prompt(scene_name.scene["next_scene"][command[1]]["module"])
+
+    elif command[0] == "go" and command[1] not in str(scene_name.scene["next_scene"]):
+        print("You can't go there.")
+
+    # if no noun
+    elif command[0] in verbs.keys() and len(command) == 1:
+        print(f"{command[0]}...?")
+    elif command[0] in scene_name.scene["nouns"] and len(command) == 1:
+        print("wha??")
+
+    # if item is in inventory
+    elif command[0] in verbs.keys() and command[1] not in scene_name.scene["nouns"].keys() and command[1] in player.stats["inventory"]:
+        output = verbs[command[0]]["func"](scene_name, command[1])
+        print(output)
+
+    # both verb and noun match
+    elif command[0] in verbs.keys() and command[1] in scene_name.scene["nouns"].keys():
+        output = verbs[command[0]]["func"](scene_name, command[1])
+        print(output)
+
+    # get or describe item inside something
+    elif [output := verbs[command[0]]["func"] (scene_name, command[1]) for i in scene_name.scene["nouns"].values() if command[1] in i["contents"] and i ["is_open"]]:
+        print(output)
+
+    elif [output := verbs[command[0]]["func"] (scene_name, command[1]) for i in scene_name.scene["nouns"].values() if command[1] in i["contents"] and not i ["is_open"]]:
+        print("You do not see such an item.")
+
+    #loop back into the prompt
+    print(colors['cyan'])
+    command_prompt(scene_name)
